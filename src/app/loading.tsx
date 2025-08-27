2dfd2d2e91f2ba93d81e6f1ec9004fb9291b67fc
developerMohib <mohibullahmohim2020@gmail.com>
2025-08-27 20:11:31 +0600

    loading page update

--- src/app/loading.tsx
+++ src/app/loading.tsx
@@ -1,7 +1,15 @@
 import React from 'react';
 
 const loading = () => {
-    return <p>Loading...</p>
+    return (
+        <div className='h-screen flex items-center justify-center'>
+            <div className="relative w-10 h-10 ">
+                <div className="absolute w-full h-full rounded-full border-2 border-orange-600 animate-ping opacity-75" />
+                <div className="relative rounded-full h-full w-full border-2 border-orange-600" />
+            </div>
+        </div>
+    )
+
 };
 
 export default loading;
